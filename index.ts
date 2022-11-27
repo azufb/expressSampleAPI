e25e47533a8809a69b8f3c88b10f3dff1135b0a2
azufb <curiousazunas@gmail.com>
2022-11-27 23:00:48 +0900

    修正

--- index.ts
+++ index.ts
@@ -6,7 +6,14 @@ const port = 3000;
 
 app.use(cors());
 
-const db = pgp('postgres://root:rootpass@127.0.0.1:5432/sampleDB')
+const db = pgp('postgres://root:rootpass@127.0.0.1:5432/sample_db');
+const connectionConfig = {
+    host: 'dpg-ce1mpa1a6gdsa63ihhq0-a',
+    port: 5432,
+    database: 'sample_db_zxg9',
+    user: 'root',
+    password: 'Y8FXbb14zGdDJZBN2tESxtOFCY7gfQIg',
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
