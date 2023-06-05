b327ed78a51ba3f778ff39a0c1dfd42b3dfc942a
azufb <curiousazunas@gmail.com>
2023-06-05 21:49:27 +0900

    不要なAPI削除

--- index.ts
+++ index.ts
@@ -24,14 +24,6 @@ const createTasksTable =
 
 db.one(createTasksTable);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Hello!!' });
-});
-
 app.post('/addTask', (req, res) => {
   const title = req.body.title;
   const sql = 'INSERT INTO tasks(title) VALUES($1)';
