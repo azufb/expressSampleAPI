9da8bfdaa02d13167e8185d2fb61353fafaef673
azufb <curiousazunas@gmail.com>
2023-06-05 22:38:45 +0900

    型指定削除

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const pgp = require('pg-promise')();
 const app = express();
-const port: number = 3000;
+const port = 3000;
 
 type ConnectionConfigType = {
   host: string | undefined;
