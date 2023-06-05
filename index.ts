5de7c8e93fd196137e251c5649d7425559e454e2
azufb <curiousazunas@gmail.com>
2023-06-05 21:53:17 +0900

    型指定忘れていた

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const pgp = require('pg-promise')();
-const app = express();
-const port = 3000;
+const express: any = require('express');
+const cors: any = require('cors');
+const pgp: any = require('pg-promise')();
+const app: any = express();
+const port: number = 3000;
+
+type ConnectionConfigType = {
+  host: string | undefined;
+  port: string | undefined;
+  database: string | undefined;
+  user: string | undefined;
+  password: string | undefined;
+};
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const connectionConfig = {
+const connectionConfig: ConnectionConfigType = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
@@ -17,46 +25,48 @@ const connectionConfig = {
   password: process.env.DB_PASSWORD,
 };
 
-const db = pgp(connectionConfig);
+const db: any = pgp(connectionConfig);
 
-const createTasksTable =
+const createTasksTable: string =
   'CREATE TABLE IF NOT EXISTS tasks (id serial PRIMARY KEY, title VARCHAR(100) NOT NULL)';
 
 db.one(createTasksTable);
 
-app.post('/addTask', (req, res) => {
-  const title = req.body.title;
-  const sql = 'INSERT INTO tasks(title) VALUES($1)';
+app.post('/addTask', (req: any, res: any) => {
+  const title: any = req.body.title;
+  const sql: string = 'INSERT INTO tasks(title) VALUES($1)';
 
-  db.manyOrNone(sql, [title]).then((data) => {
+  db.manyOrNone(sql, [title]).then((data: any) => {
     console.log(data);
     res.send(data);
   });
 });
 
-app.post('/addTasks', (req, res) => {
-  const columnSet = new pgp.helpers.ColumnSet(['title'], { table: 'tasks' });
-  const query = pgp.helpers.insert(req.body.sample, columnSet);
+app.post('/addTasks', (req: any, res: any) => {
+  const columnSet: any = new pgp.helpers.ColumnSet(['title'], {
+    table: 'tasks',
+  });
+  const query: any = pgp.helpers.insert(req.body.sample, columnSet);
 
-  db.manyOrNone(query).then((data) => {
+  db.manyOrNone(query).then((data: any) => {
     console.log(data);
     res.send(data);
   });
 });
 
-app.get('/getTasks', (req, res) => {
-  const sql = 'SELECT * FROM tasks';
+app.get('/getTasks', (req: any, res: any) => {
+  const sql: string = 'SELECT * FROM tasks';
 
-  db.manyOrNone(sql).then((data) => {
+  db.manyOrNone(sql).then((data: any) => {
     res.send(data);
   });
 });
 
-app.post('/deleteTask', (req, res) => {
-  const targetId = req.body.id;
-  const sql = 'DELETE FROM tasks WHERE id = $1';
+app.post('/deleteTask', (req: any, res: any) => {
+  const targetId: any = req.body.id;
+  const sql: string = 'DELETE FROM tasks WHERE id = $1';
 
-  db.manyOrNone(sql, [targetId]).then((data) => {
+  db.manyOrNone(sql, [targetId]).then((data: any) => {
     res.send(data);
   });
 });
